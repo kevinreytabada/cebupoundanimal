bb1fbf8249661b3d7abed7ff153983ec1741ca65
Kevin Rey Tabada <reytabs1993@gmail.com>
2017-09-23 18:49:52 +0800

    ...

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
+        "id": "cordova-plugin-fcm.FCMPlugin",
+        "pluginId": "cordova-plugin-fcm",
+        "clobbers": [
+            "FCMPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-fcm": "2.1.2"
+}
+// BOTTOM OF METADATA
+});
